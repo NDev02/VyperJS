b6c451911f3cf1f221a3bb4ce56f291f8d096c4d
Nathan Gordon <ndev02@outlook.com>
2017-08-17 21:22:03 -0400

    Minor tweak

--- VyperJS v0.0.3.3/JavaScript/vyper_project.js
+++ VyperJS v0.0.3.3/JavaScript/vyper_project.js
@@ -3,15 +3,17 @@
 *  Monday August 14th, 11:09:31
 *  ©2017-2018 Nathan Gordon
 */
-var x = 250;
-var y = 250;
+var x = (window.innerWidth-25)/2;
+var y = (window.innerHeight-25)/2;
 var speed = 10;
 
 var rows = 10;
 var cols = 10;
 
+var decayRate = 1;
+
 function Setup(){
-  Canvas.createCanvas(500,500);
+  Canvas.createCanvas(window.innerWidth-25, window.innerHeight-25);
   //Canvas.background(new Color(0,255,0,255));
 }
 
@@ -44,9 +46,9 @@ function Draw(){
     y = 0;
   }
   var radius = Math.random() * 50;
-  Shapes2D.setFill(new Color(100,100,100,2));
+  Shapes2D.setFill(new Color(100,100,100,decayRate));
   Shapes2D.rect(0, 0, Canvas.width, Canvas.height);
-  Shapes2D.setFill(VyColor.randomColor(new Color(50,255,100,200)));
+  Shapes2D.setFill(VyColor.randomColor(new Color(50,255,100,100)));
   Shapes2D.ellipse(x,y,Math.random() * 50);
   Shapes2D.setFont("Sedgwick Ave Display", 80);
   Shapes2D.setFill(VyColor.randomColor(new Color(0,0,100,255)));
@@ -57,6 +59,6 @@ function Draw(){
   Shapes2D.text("VyperJS", new VyVector(12, 103), "STROKE");
   Shapes2D.setFont("Sedgwick Ave Display", 50);
   Shapes2D.setFill(VyColor.randomColor(new Color(0,255,0,255)));
-  Shapes2D.text("v0.0.3.3", new VyVector(300, 450), "FILL");
-  Shapes2D.text("v0.0.3.3", new VyVector(303, 452), "STROKE");
+  Shapes2D.text("v0.0.3.3", new VyVector(15, 215), "FILL");
+  Shapes2D.text("v0.0.3.3", new VyVector(12, 217), "STROKE");
 }
